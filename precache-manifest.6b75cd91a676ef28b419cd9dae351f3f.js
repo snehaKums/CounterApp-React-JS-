7f7955a607f327a3bed95ee382b19be077a51850
Kumari <sneha.kumari@capgemini.com>
2020-09-28 17:10:07 +0530

    Updates

---
+++ precache-manifest.6b75cd91a676ef28b419cd9dae351f3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb106b0607028d3d8801ecfd01f14746",
+    "url": "/CounterApp-React-JS-/index.html"
+  },
+  {
+    "revision": "4bf0610347e517b984c8",
+    "url": "/CounterApp-React-JS-/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "2ee95f9e3aa23b828254",
+    "url": "/CounterApp-React-JS-/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4bf0610347e517b984c8",
+    "url": "/CounterApp-React-JS-/static/js/2.c86f8507.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CounterApp-React-JS-/static/js/2.c86f8507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee95f9e3aa23b828254",
+    "url": "/CounterApp-React-JS-/static/js/main.af1d9c60.chunk.js"
+  },
+  {
+    "revision": "142dea295512c3fbb1ea",
+    "url": "/CounterApp-React-JS-/static/js/runtime-main.25f82eff.js"
+  }
+]);
